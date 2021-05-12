f18a81fcf723221443de7a080763cc34e2d60ea8
djvuelo <dj.vuelo@gmail.com>
2021-05-12 21:42:18 +0400

    Test

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def print_bye(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
