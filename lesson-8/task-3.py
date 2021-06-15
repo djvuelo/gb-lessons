c23ec0f6f7b5ea09b95a1eee6622a801165a55be
djvuelo <dj.vuelo@gmail.com>
2021-06-15 22:48:52 +0300

    Fix

--- lesson-8/task-3.py
+++ lesson-8/task-3.py
@@ -6,9 +6,10 @@ def type_logger(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         _func_args = ', '.join(map(lambda val: f'{val}: {str(type(val))}', [*args]))
-        _func_kwargs = ', '.join(map(lambda val: f'{val[0]}: {str(type(val[1]))}', [*kwargs.items()]))
+        _func_kwargs = ', '.join(map(lambda val: f'{val[1]}: {str(type(val[1]))}', [*kwargs.items()]))
         _func_all = ', '.join([_func_args, _func_kwargs])
-        return f'{func.__name__}({_func_all})  ->  {result}: {type(result)}'
+        print(f'{func.__name__}({_func_all})  ->  {result}: {type(result)}')
+        return result
 
     return wrapper
 
