06a56ba8dcc56e50fb772a05fa6a8a9b48addb5b
djvuelo <dj.vuelo@gmail.com>
2021-06-21 17:34:54 +0300

    Task 1 add method __add__

--- lesson-10/task_1.py
+++ lesson-10/task_1.py
@@ -3,12 +3,21 @@ class Matrix:
         self.matrix = matrix
 
     def __str__(self):
-        return '\n'.join(map(lambda line: ' '.join(map(str, line)), self.matrix)) + '\n' + '-' * 10
+        return '\n'.join(map(lambda line: ' '.join(map(str, line)), self.matrix))
+
+    def __add__(self, other):
+        new_matrix = []
+        for i in range(0, len(self.matrix)):
+            new_matrix.append([])
+            for j in range(0, len(self.matrix[0])):
+                new_matrix[i].append(self.matrix[i][j] + other.matrix[i][j])
+        return Matrix(new_matrix)
 
 
 matrix1 = Matrix([[31, 22], [37, 43], [51, 86]])
-matrix2 = Matrix([[3, 5, 32], [2, 4, 6], [-1, 64, -8]])
-matrix3 = Matrix([[3, 5, 8, 3], [8, 3, 7, 1]])
+matrix2 = Matrix([[3, 5], [2, 4], [-1, 64]])
 print(matrix1)
+print('+')
 print(matrix2)
-print(matrix3)
+print('=')
+print(f'{matrix1 + matrix2}')
